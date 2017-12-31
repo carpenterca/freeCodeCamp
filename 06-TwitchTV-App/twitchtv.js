6b8f8b0e6049b9623ecf38cce81eef5d54bcd5d0
Cory Carpenter <carpenterca@g.cofc.edu>
2017-12-31 13:31:13 -0500

    Update desktop card appearance

--- 06-TwitchTV-App/twitchtv.js
+++ 06-TwitchTV-App/twitchtv.js
@@ -1,5 +1,4 @@
-const streamers = ["freecodecamp", "day9tv", "kinenz", "boogie2988", "novawar", "lagtvmaximusblack", "technicalalpha", "disguisedtoasths", "trumpsc", "itmejp", "towelliee", "asmongold", "anniefuchsia"];
-//funfunfunction, Faker
+const streamers = ["wunyu", "Quin69", "Sco", "GingiTV", "imaqtpie", "Faker", "freecodecamp", "funfunfunction", "day9tv", "kinenz", "boogie2988", "novawar", "lagtvmaximusblack", "technicalalpha", "disguisedtoasths", "trumpsc", "itmejp", "towelliee", "asmongold", "anniefuchsia"];
 let liveStreams = []; // if live add to array, if present in offlineStreams then remove from array
 let offlineStreams = [];
 let callsCompleted = 0;
@@ -54,10 +53,10 @@ function getLiveStreamers(streamerName) {
       callsCompleted++;
       if (currentView === "home" && callsCompleted === streamers.length) {
         //console.log(offlineStreams);
-        setUpOfflineStreamers();
+        getOfflineStreamers();
       } else if (currentView === "search") {
         //console.log(offlineStreams);
-        setUpOfflineStreamers();
+        getOfflineStreamers();
       }
     }
   });
@@ -71,7 +70,10 @@ function getOfflineStreamers() {
       url: userURL,
       dataType: 'json',
       success: function(jsonData) {
-        displayOfflineStreamers(jsonData, offlineStreams[i]);
+        // check for bad accounts
+        if (checkAccountStatus(jsonData)) {
+          displayOfflineStreamers(jsonData, offlineStreams[i]);
+        }
       },
       error: function() {
         displayCallError();
@@ -80,63 +82,80 @@ function getOfflineStreamers() {
   }
 }
 
-function setUpOfflineStreamers() {
-  for (let j = 0; j < offlineStreams.length; j++) {
-    $("div#offline-streamers").append('<div class="streamer" id="' + offlineStreams[j] + '"></div>');
-    $("#" + offlineStreams[j]).append('<div class="standard-view" id="standard-view-' + offlineStreams[j] + '"></div>');
-  }
-  getOfflineStreamers();
-}
-
 function displayLiveStreamers(jsonData, streamerName) {
   let timestamp = new Date().toLocaleString();
   $("div#online-streamers").append('<div class="streamer" id="' + streamerName + '"></div>');
-  $("#" + streamerName).append('<a class="stream-link" target="_blank" href="' + jsonData.stream['channel'].url + '"><div class="standard-view" id="standard-view-' + streamerName + '"></div></a>');
+  $("#" + streamerName).append('<a class="stream-link-top" target="_blank" href="' + jsonData.stream['channel'].url + '"><div class="standard-view" id="standard-view-' + streamerName + '"></div></a>');
   $("#standard-view-" + streamerName).append('<img class="user-img" src="' + jsonData.stream['channel'].logo + '"></img>');
   $("#standard-view-" + streamerName).append('<div class="standard-view-text" id="standard-view-text-' + streamerName + '"></div>');
   $("#standard-view-text-" + streamerName).append('<h2 class="streamer-name" id="user-' + streamerName + '">' + jsonData.stream['channel'].display_name + '</h2>');
   $("#standard-view-text-" + streamerName).append('<p class="streamer-status" id="status-' + streamerName + '"><span class="fa fa-circle" aria-hidden="true"></span>' + "&nbsp;&nbsp;LIVE" + '</p>');
   $("#standard-view-" + streamerName).append('<div style="clear: both;"></div>');
   $("#" + streamerName).append('<p class="streamer-playing" id="playing-' + streamerName + '"></p>');
-  //$("div#" + streamers[i]).append('<p class="stream-description" id="decscription-' + streamers[i] + '">' + jsonData.stream[i]['channel'].description + '</p>');
-  $("#" + streamerName).append('<a class="stream-link" target="_blank" href="' + jsonData.stream['channel'].url + '"><img class="stream-preview-img" src="' + jsonData.stream['preview'].medium + '?lastmod=' + timestamp + '"></img></a>');
+  $("#" + streamerName).append('<a class="stream-link" target="_blank" href="' + jsonData.stream['channel'].url + '"><img class="stream-preview-img" src="' + jsonData.stream['preview'].large + '?lastmod=' + timestamp + '"></img></a>');
 
   $("#playing-" + streamerName).append('<p class="stream-game" id="game-' + streamerName + '">Playing: ' + jsonData.stream['channel'].game + '</p>');
-  $("#playing-" + streamerName).append('<p class="stream-status" id="title-' + streamerName + '">\"' + jsonData.stream['channel'].status + '\"</p>');
+  // limit stream title to 2 lines
+  let streamTitle = jsonData.stream['channel'].status;
+  if (jsonData.stream['channel'].status.length > 69) {
+    streamTitle = streamTitle.toString().slice(0, 63) + "...";
+  }
+  $("#playing-" + streamerName).append('<p class="stream-status" id="title-' + streamerName + '">\"' + streamTitle + '\"</p>');
   $("#playing-" + streamerName).append('<p class="stream-views" id="viewer-count-' + streamerName + '">Viewers: ' + jsonData.stream.viewers + '</p>');
 }
 
 function displayOfflineStreamers(jsonData, streamerName) {
-  if (jsonData.status === 422) {
-    $("div#online-streamers").html("");
-    $("div#offline-streamers").html("");
-    $("#error-info").html("Account has been deleted.").show();
-  }
-  else if (jsonData.status !== 404) {
-    //console.log(jsonData);
-    $("#standard-view-" + streamerName).append('<img class="user-img" src="' + jsonData.logo + '"></img>');
-    $("#standard-view-" + streamerName).append('<a class="stream-link" target="_blank" href="https://www.twitch.tv/' + streamerName + '"><div class="standard-view-text" id="standard-view-text-' + streamerName + '"></div></a>');
-    $("#standard-view-text-" + streamerName).append('<h2 class="streamer-name" id="user-' + streamerName + '">' + jsonData.display_name + '</h2>');
-    $("#standard-view-text-" + streamerName).append('<p class="streamer-status" id="status-' + streamerName + '">' + "Offline" + '</p>');
-    $("#standard-view-" + streamerName).append('<div style="clear: both;"></div>');
-    if (jsonData.bio !== null) {
-      $("div#" + streamerName).append('<p class="stream-description" id="decscription-' + streamerName + '">' + jsonData.bio + '</p>');
-    } else {
+  //console.log(jsonData);
+  $("div#offline-streamers").append('<div class="streamer" id="' + streamerName + '"></div>');
+  $("#" + streamerName).append('<div class="standard-view" id="standard-view-' + streamerName + '"></div>');
+  $("#standard-view-" + streamerName).append('<img class="user-img" src="' + jsonData.logo + '"></img>');
+  $("#standard-view-" + streamerName).append('<a class="stream-link-top" target="_blank" href="https://www.twitch.tv/' + streamerName + '"><div class="standard-view-text" id="standard-view-text-' + streamerName + '"></div></a>');
+  $("#standard-view-text-" + streamerName).append('<h2 class="streamer-name" id="user-' + streamerName + '">' + jsonData.display_name + '</h2>');
+  $("#standard-view-text-" + streamerName).append('<p class="streamer-status" id="status-' + streamerName + '">' + "Offline" + '</p>');
+  $("#standard-view-" + streamerName).append('<div style="clear: both;"></div>');
+  //if (jsonData.bio !== null) {
+  if (jsonData.bio !== null) {
+    $("div#" + streamerName).append('<p class="stream-description" id="decscription-' + streamerName + '">' + jsonData.bio + '</p>');
+    if (currentView === "search") {
+      $("#" + streamerName).css("min-height", "50px");
+    }
+  } else {
+    if (currentView === "search") {
       // remove border sperating bio if there is no bio given
       $("#standard-view-" + streamerName).removeClass("standard-view");
       $("#standard-view-" + streamerName).addClass("standard-view-nobio");
+      $("#" + streamerName).css("min-height", "50px");
     }
-  } else {
-    $("div#online-streamers").html("");
-    $("div#offline-streamers").html("");
-    $("#error-info").html("Account does not exist.<br><br>Check the spelling and try again.").show();
   }
 }
 
 function displayCallError() {
-  $("div#online-streamers").html("");
-  $("div#offline-streamers").html("");
+  $("div#streamers").html("");
   $("div#streamers").append('<p>' + "Data Not Retrieved." + '</p>');
+}
+
+function checkAccountStatus(jsonData) {
+  if (jsonData.status === 400) {
+    $("div#online-streamers").html("");
+    $("div#offline-streamers").html("");
+    $("#error-info").html("Invalid search.<br><br>Spaces are not valid in account names.").show();
+    return false;
+  }
+  else if (jsonData.status === 404) {
+    $("div#online-streamers").html("");
+    $("div#offline-streamers").html("");
+    $("#error-info").html("Account does not exist.<br><br>Check the spelling and try again.").show();
+    return false;
+  }
+  else if (jsonData.status === 422) {
+    $("div#online-streamers").html("");
+    $("div#offline-streamers").html("");
+    $("#error-info").html("Account has been deleted.").show();
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 function searchStreamer() {
@@ -172,6 +191,10 @@ function showOfflineStreams() {
   $("#online-streamers").hide();
   $("#online-title").hide();
 }
+
+/*function trimStreamTitle(str) {
+
+}*/
 
 // to debug time sensitive code
 function startTime() {
